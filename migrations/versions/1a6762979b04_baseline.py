68023000b4a23e5198c12ce86daa98664c24d833
Jonas <dockxjonas@gmail.com>
2024-12-20 20:28:43 +0000

    Cleaning up the flask database

---
+++ migrations/versions/1a6762979b04_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 1a6762979b04
+Revises: 
+Create Date: 2024-12-20 20:27:30.976125
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1a6762979b04'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
